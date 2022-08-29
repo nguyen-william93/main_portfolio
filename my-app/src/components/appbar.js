95b14e030736bae19fa266584c87ba22fac38e7e
nguyen-william93 <=>
2022-08-29 16:34:01 -0500

    add bootstrap and appbar.js

---
+++ my-app/src/components/appbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Intent from './link'
+
+export default function Appbar (){
+    return (
+        <div className='container my-3'>
+            <ul className = 'nav justify-content-center'>
+                <li className = 'nav-item mx-4'>
+                    <Intent to='/'>Home</Intent>
+                </li>
+                <li className='nav-item mx-4'>
+                    <Intent to='/projects'>Projects</Intent>
+                </li>
+                <li className='nav-item mx-4'>
+                    <Intent to='/blog'>Blog</Intent>
+                </li>
+            </ul>
+        </div>
+    )
+}
