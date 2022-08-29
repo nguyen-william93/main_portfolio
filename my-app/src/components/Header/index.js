7e7e13405636aad740899c01050ed7f274ac6844
nguyen-william93 <=>
2022-08-29 18:08:50 -0500

    add content to main page and animation to header

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -1,18 +1,28 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 export default function Header (){
+
     return (
         <div className='container my-3'>
             <ul className = 'nav justify-content-center'>
                 <li className = 'nav-item mx-4'>
-                    <Link to='/'>Home</Link>
+                    <NavLink to="/" style={({ isActive }) => ({ 
+                         color: isActive ? 'white' : 'rgb(13,110,253)' })}>
+                        Home
+                    </NavLink>
                 </li>
                 <li className='nav-item mx-4'>
-                    <Link to='/projects'>Projects</Link>
+                    <NavLink to="/projects" style={({ isActive }) => ({ 
+                         color: isActive ? 'white' : 'rgb(13,110,253)' })}>
+                        Projects
+                    </NavLink>
                 </li>
                 <li className='nav-item mx-4'>
-                    <Link to='/blog'>Blog</Link>
+                    <NavLink to="/blog" style={({ isActive }) => ({ 
+                         color: isActive ? 'white' : 'rgb(13,110,253)' })}>
+                        Blog
+                    </NavLink>
                 </li>
             </ul>
         </div>
---
+++ my-app/src/components/Info/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div className="container">
+      <div className="my-5"></div>
+      <div className="display-3" style={{ color: "white" }}>
+        <span style={{ color: "white" }}>Hello, my name is</span> <br />
+        William.
+      </div>
+      <div style={{ color: "rgb(6, 199, 6)" }} className="h1 code mt-4 mb-3">
+        {"{"}
+      </div>
+      <div className="mx-5 my-4 h3 text-justify info">
+        I really enjoy coding and learning new things.
+        <br /> I strive to always learn more and improve as much as possible so
+        that I can grow as a Developer and person.
+      </div>
+      <div style={{ color: "rgb(6, 199, 6)" }} className="h1 code mt-2 mb-3">
+        {"}"}
+      </div>
+    </div>
+  )
+}
