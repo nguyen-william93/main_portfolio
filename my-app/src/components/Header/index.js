a5a8b45d78c00ad2f3c716d86001cd233ecf1db3
nguyen-william93 <=>
2022-08-30 15:55:20 -0500

    add projects.yaml

--- my-app/src/components/Header/index.js
+++ my-app/src/components/Header/index.js
@@ -4,8 +4,8 @@ import {NavLink} from 'react-router-dom'
 export default function Header (){
 
     return (
-        <div className='container my-3'>
-            <ul className = 'nav justify-content-center'>
+        <div className='container my-3 navbar justify-content-center'>
+            <ul className = 'nav justify-content-center header'>
                 <li className = 'nav-item mx-4'>
                     <NavLink to="/" style={({ isActive }) => ({ 
                          color: isActive ? 'white' : 'grey' })}>
