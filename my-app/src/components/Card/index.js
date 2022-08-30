09d372d701e529a790447086e870e228640dca7d
nguyen-william93 <=>
2022-08-30 17:43:45 -0500

    moving project to a constant file

--- my-app/src/components/Card/index.js
+++ my-app/src/components/Card/index.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 const Card = (props) => {
     const {cardTitle, cardSubtitle, cardLink, cardStack, cardImage} = props
-    console.log(cardLink)
+    // console.log(cardLink)
     
     return (
         <div className="col-lg-4 d-flex align-items-stretch">
             <div className="card mx-3 my-3">
-            <img class="card-img-top" src={cardImage} alt="Card image cap"/>
+            <img className="card-img-top" src={cardImage} alt="a"/>
                 <div className="card-body">
                 <h2 className="card-title">{cardTitle}</h2>
                 <p className="card-text">{cardSubtitle}</p>
                 <p className="card-text">Technology use: {cardStack}</p>
             </div>
             <ul className='d-flex justify-content-around px-0'>
-                <a href={cardLink} className="card-link" target="_blank">
+                <a href={cardLink} className="card-link" target="_blank" rel="noopener noreferrer">
                 View Demo
                 </a>
-                <a href={cardLink} className="card-link" target="_blank">
+                <a href={cardLink} className="card-link" target="_blank" rel="noopener noreferrer">
                 View Source Code
                 </a>
             </ul>
--- my-app/src/pages/Projects.js
+++ my-app/src/pages/Projects.js
@@ -1,24 +1,9 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Card from '../components/Card'
+import {projects} from '../constant/constants'
 
 const Projects = () => {
-    const projects = [
-    {title: "Google Book Search",
-    subtitle: "A react full stack app that allowed you to search for a book and its detail. User can create an account, save and delete their list.",
-    link: "https://hidden-sierra-28467.herokuapp.com/",
-    stack: "Mongo, React, Javascript, Express, Node",
-    image: "/images/google-book.png",
-    id: '0'
-    },
-    
-    {title: "Tic-Tac-Toe",
-    subtitle: "A beautiful version of Tic-Tac-Toe",
-    link: "https://nguyen-william93.github.io/Tic-Tac-Toe/",
-    stack: "HTML, Javascript, CSS",
-    image: "/images/tictactoe.png",
-    id: "1"
-    }]
-
+    // console.log(projects)
     return (
         <div className='container-fluid'>
             <div className='row'>
